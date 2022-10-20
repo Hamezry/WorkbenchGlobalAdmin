cf0ecd3c4381cced6c0f8d45fbc37f2493f3ff26
Hemense Lan <lihemen@gmail.com>
2022-10-20 17:07:44 +0100

    card fixes

--- src/pages/products/components/tile/index.jsx
+++ src/pages/products/components/tile/index.jsx
@@ -31,14 +31,12 @@ const GlobalProductTile = () => {
             <div className='flex px-6 w-full justify-between items-center'>
               <span className='w-1 h-20 absolute left-0 top-6 bg-[#FA5A7D] rounded-r-3xl'></span>
               <div className='flex flex-col space-y-8'>
-                <p className=' text-[#47494E] text-[16px] pt-3'>
-                  Total Products
-                </p>
+                <p className=' text-[#47494E] text-[16px] pt-3'>Products</p>
                 <span className='text-[25px] font-bold '>
                   {formatter(cardData.total_products.value)}
                 </span>
               </div>
-              <span className='bg-[#FA5A7D] rounded-full p-3 flex items-center justify-center'>
+              <span className='bg-[#FA5A7D] rounded-full p-4 flex items-center justify-center'>
                 <span className='bg-white rounded-sm p-1'>
                   <HiChartBar className='text-[#FA5A7D] text-lg' />
                 </span>
@@ -67,26 +65,24 @@ const GlobalProductTile = () => {
         ) : (
           <div className='flex flex-col py-6 bg-[#FFFFFF] rounded-3xl flex-1 space-y-6 items-center relative'>
             <div className='flex px-6 w-full justify-between items-center'>
-              <span className='w-1 h-20 absolute left-0 top-6 bg-[#FA5A7D] rounded-r-3xl'></span>
+              <span className='w-1 h-20 absolute left-0 top-6 bg-[#ff5630] rounded-r-3xl'></span>
               <div className='flex flex-col space-y-8'>
-                <p className=' text-[#47494E] text-[16px] pt-3'>
-                  Products (Commodity)
-                </p>
+                <p className=' text-[#47494E] text-[16px] pt-3'>Commodity</p>
                 <span className='text-[25px] font-bold '>
                   {formatter(cardData.commodities.value)}
                 </span>
               </div>
-              <span className='bg-[#FA5A7D] rounded-full p-3 flex items-center justify-center'>
+              <span className='bg-[#ff5630] rounded-full p-4 flex items-center justify-center'>
                 <span className='bg-white rounded-sm p-1'>
-                  <HiChartBar className='text-[#FA5A7D] text-lg' />
+                  <HiChartBar className='text-[#ff5630] text-lg' />
                 </span>
               </span>
             </div>
 
             <div className='flex items-center w-full'>
               <p className='flex items-center justify-between px-6 space-x-3'>
-                <span className=' bg-[#fae3e7] p-1 rounded-full'>
-                  <HiOutlineArrowUpRight className=' text-[#FA5A7D] text-base font-bold ' />
+                <span className=' bg-[#fef1ed] p-1 rounded-full'>
+                  <HiOutlineArrowUpRight className=' text-[#ff5630] text-base font-bold ' />
                 </span>
                 <p className='text-sm space-x-4 flex items-center'>
                   <span>Last Added:</span>
@@ -103,26 +99,24 @@ const GlobalProductTile = () => {
         ) : (
           <div className='flex flex-col py-6 bg-[#FFFFFF] rounded-3xl flex-1 space-y-6 items-center relative'>
             <div className='flex px-6 w-full justify-between items-center'>
-              <span className='w-1 h-20 absolute left-0 top-6 bg-[#FF5630] rounded-r-3xl'></span>
+              <span className='w-1 h-20 absolute left-0 top-6 bg-[#559BB1] rounded-r-3xl'></span>
               <div className='flex flex-col space-y-8'>
-                <p className=' text-[#47494E] text-[16px] pt-3'>
-                  Products (Input)
-                </p>
+                <p className=' text-[#47494E] text-[16px] pt-3'>Input</p>
                 <span className='text-[25px] font-bold '>
                   {formatter(cardData.inputs.value)}
                 </span>
               </div>
-              <span className='bg-[#FF5630] rounded-full p-3 flex items-center justify-center'>
+              <span className='bg-[#559BB1] rounded-full p-4 flex items-center justify-center'>
                 <span className='bg-white rounded-sm p-1'>
-                  <HiChartBar className='text-[#FF5630] text-lg' />
+                  <HiChartBar className='text-[#559BB1] text-lg' />
                 </span>
               </span>
             </div>
 
             <div className='flex items-center w-full'>
               <p className='flex items-center justify-between px-6 space-x-3'>
-                <span className=' bg-[#fae3e7] p-1 rounded-full'>
-                  <HiOutlineArrowUpRight className=' text-[#FA5A7D] text-base font-bold ' />
+                <span className=' bg-[#e7eff1] p-1 rounded-full'>
+                  <HiOutlineArrowUpRight className=' text-[#559BB1] text-base font-bold ' />
                 </span>
                 <p className='text-sm space-x-4 flex items-center'>
                   <span>Last Added:</span>
@@ -139,26 +133,24 @@ const GlobalProductTile = () => {
         ) : (
           <div className='flex flex-col py-6 bg-[#FFFFFF] rounded-3xl flex-1 space-y-6 items-center relative'>
             <div className='flex px-6 w-full justify-between items-center'>
-              <span className='w-1 h-20 absolute left-0 top-6 bg-[#FF5630] rounded-r-3xl'></span>
+              <span className='w-1 h-20 absolute left-0 top-6 bg-[#3F6A83] rounded-r-3xl'></span>
               <div className='flex flex-col space-y-8'>
-                <p className=' text-[#47494E] text-[16px] pt-3'>
-                  Products (Fees)
-                </p>
+                <p className=' text-[#47494E] text-[16px] pt-3'>Fees</p>
                 <span className='text-[25px] font-bold '>
                   {formatter(cardData.fees.value)}
                 </span>
               </div>
-              <span className='bg-[#FF5630] rounded-full p-3 flex items-center justify-center'>
+              <span className='bg-[#3F6A83] rounded-full p-4 flex items-center justify-center'>
                 <span className='bg-white rounded-sm p-1'>
-                  <HiChartBar className='text-[#FF5630] text-lg' />
+                  <HiChartBar className='text-[#3F6A83] text-lg' />
                 </span>
               </span>
             </div>
 
             <div className='flex items-center w-full'>
               <p className='flex items-center justify-between px-6 space-x-3'>
-                <span className=' bg-[#fae3e7] p-1 rounded-full'>
-                  <HiOutlineArrowUpRight className=' text-[#FA5A7D] text-base font-bold ' />
+                <span className=' bg-[#ecf5fb] p-1 rounded-full'>
+                  <HiOutlineArrowUpRight className=' text-[#3F6A83] text-base font-bold ' />
                 </span>
                 <p className='text-sm space-x-4 flex items-center'>
                   <span>Last Added:</span>

5b39986cbd1edc2d2820c74aff0f748fccfc15fd
Hemense Lan <lihemen@gmail.com>
2022-10-20 15:25:30 +0100

    Routes file, country tile update

---
+++ src/Routes.js
@@ -0,0 +1,50 @@
+// A Routing declariation File
+
+const Routes = [
+  {
+    route: 'Tenants',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adip',
+    link: 'tenants',
+    children: [
+      {
+        route: '',
+        subtitle: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    route: 'Prouducts',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adip',
+    link: 'products',
+    children: [
+      {
+        route: '',
+        subtitle: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    route: 'Countries',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adip',
+    link: 'countries',
+    children: [
+      {
+        route: 'Nigeria',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adip',
+        link: '163',
+      },
+      {
+        route: 'Kenya',
+        subtitle: 'Lorem ipsum dolor sit amet, consectet adip',
+        link: '119',
+      },
+      {
+        route: 'China',
+        subtitle: 'Lorem ipsum dolor sit amet, consectet adip',
+        link: '49',
+      },
+    ],
+  },
+];
